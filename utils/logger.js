bd5b62a5c0659ca2c3ba0480f3f2de3545567396
Hugo <827920340@qq.com>
2026-02-10 18:02:34 +0800

    feat: 增加日志脫敏功能,確保敏感數據不外漏

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,23 @@
 import pino from "pino";
 
 const logger = pino({
+  redact: {
+    paths: [
+      "password",
+      "req.headers.authorization",
+      "user.email",
+      "*.password",
+      "data.token",
+      "token",
+    ],
+
+    placeholder: "*** [SENSITIVE DATA MASKED] ***",
+  },
   transport: {
     target: "pino-pretty",
     options: {
       colorize: true,
-      translateTime: "SYS:standard",
+      translateTime: "yyyy-mm-dd HH:MM:ss.l o",
     },
   },
 });
